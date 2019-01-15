d72952990cfe9b36ce5d6cfbf0f63d9ea516edb0
aishwarya_g <aishu047@gmail.com>
2019-01-15 18:13:33 +0530

    Initial commmit Task2

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -3,12 +3,15 @@
 package com.stackroute.domain;
 
 public class Movie {
+    public Movie(){}
+    public Movie(Actor actor){
+        this.actor = actor;
+    }
     public void setActor(Actor actor) {
         this.actor = actor;
     }
 
     Actor actor;
-
 
     public Actor getActor() {
         return actor;
