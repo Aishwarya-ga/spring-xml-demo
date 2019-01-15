22e62fb598275ff308c081bb5cf2f1944020b2a9
aishwarya_g <aishu047@gmail.com>
2019-01-15 16:24:15 +0530

    Initial commmit Task1

---
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -0,0 +1,45 @@
+/*Actor has two String properties, name and gender, and an age property of type int.
+*An Actor can be initialized with the three properties via the corresponding setter methods. Use
+*property based injection in the bean definition file (beans.xml)
+*/
+
+package com.stackroute.domain;
+
+public class Actor {
+    private String name;
+    private String gender;
+    private int age;
+
+    public Actor(){}
+    public Actor(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,16 @@
+/*Movie “has a” Actor that can be initialized via the corresponding setter method. Use property
+* based object injection in the bean definition file (beans.xml)*/
+package com.stackroute.domain;
+
+public class Movie {
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    Actor actor;
+
+
+    public Actor getActor() {
+        return actor;
+    }
+}
