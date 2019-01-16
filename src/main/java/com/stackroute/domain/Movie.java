f1d6b7499d6dd38d528dcc85fb4f7af6341a868d
aishwarya_g <aishu047@gmail.com>
2019-01-16 11:20:09 +0530

    modified files

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
+public class Movie {
     Actor actor;
     public Movie(){}
     public Movie(Actor actor){
@@ -21,18 +21,5 @@ public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactor
     public Actor getActor() {
         return actor;
     }
-    //implementing method in ApplicationContextAware
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        Movie movie = (Movie) applicationContext.getBean("movie");
-        System.out.println("From application context: "+movie.getActor().getName()+" "+movie.getActor().getGender()+" "+movie.getActor().getAge());
-    }
-    //implementing method in BeanNameAware
-    public void setBeanName(String beanName){
-        System.out.println("Bean name: "+" "+ beanName);
-    }
-    //implementing method in BeanFactoryAware
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        Movie movie = (Movie) beanFactory.getBean("movie");
-        System.out.println("From beanFactory: " + movie.getActor().getName() + " " + movie.getActor().getAge() + " " + movie.getActor().getGender());
-    }
+
 }
